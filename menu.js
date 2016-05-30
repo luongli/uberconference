09b0d593c596cb817098ba2d81a048f9be73c7b9
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-30 18:16:18 +0700

    Add: user menu

--- menu.js
+++ menu.js
@@ -37,3 +37,36 @@ function hideAppMenu() {
 
     menu.style.display = "none";
 }
+
+
+function showUserMenu() {
+    var menu = document.getElementById("user-menu"); 
+    menu.style.display = "block";
+    var id = setInterval(frame, 5);
+    var top = -200;
+    function frame() {
+        if (top == 60) {
+            return;
+        } else {
+            top += 10; 
+            menu.style.top = top + 'px';  
+        }
+    }
+}
+
+function hideUserMenu() {
+	console.log("activated");
+	var menu = document.getElementById("user-menu"); 
+    var id = setInterval(frame, 100);
+    var top = -200;
+    function frame() {
+        if (top == -200) {
+            return;
+        } else {
+            top -= 10; 
+            menu.style.top = top + 'px';  
+        }
+    }
+
+    menu.style.display = "none";
+}
