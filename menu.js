fc3fe62264199aa60ad5ef895ceaf39cdab99ac1
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-30 18:10:28 +0700

    Add: AppMenu

---
+++ menu.js
@@ -0,0 +1,39 @@
+function showSubmenu() {
+	document.getElementById("submenu").style.display = "block";
+}
+
+function hideSubmenu() {
+	document.getElementById("submenu").style.display = "none";
+}
+
+function showAppMenu() {
+    var menu = document.getElementById("app-menu"); 
+    menu.style.display = "block";
+    var id = setInterval(frame, 5);
+    var top = -200;
+    function frame() {
+        if (top == 60) {
+            return;
+        } else {
+            top += 10; 
+            menu.style.top = top + 'px';  
+        }
+    }
+}
+
+function hideAppMenu() {
+	console.log("activated");
+	var menu = document.getElementById("app-menu"); 
+    var id = setInterval(frame, 100);
+    var top = -200;
+    function frame() {
+        if (top == -200) {
+            return;
+        } else {
+            top -= 10; 
+            menu.style.top = top + 'px';  
+        }
+    }
+
+    menu.style.display = "none";
+}
